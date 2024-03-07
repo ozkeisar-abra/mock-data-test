8ba59168e3c4310124b986601eee1c5d52ad75d2
ozkeisar-abra <135840920+ozkeisar-abra@users.noreply.github.com>
2024-03-08 00:01:50 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,31 +1,35 @@
-const routes = [
-  {
-    routePath: '/route',
-    method: 'get',
-    description: 'example route for stub-on-prem',
-    activeResponseIndex: 1,
-    responses: [
-      {
-        name: 'object response example',
-        description: 'return example of object response ',
-        res: {
-          data: {
-            isObj: true,
+const parent = {
+  filename: 'example',
+  path: '/example',
+  routes : [
+    {
+      routePath: '/route',
+      method: 'get',
+      description: 'example route for stub-on-prem',
+      activeResponseIndex: 1,
+      responses: [
+        {
+          name: 'object response example',
+          description: 'return example of object response ',
+          res: {
+            data: {
+              isObj: true,
+            },
+            code: 200,
           },
-          code: 200,
+          type: 'obj',
         },
-        type: 'obj',
-      },
-      {
-        name: 'func response example',
-        description: 'example of func that return object response',
-        exec: (req) => {
-          return { data: { isFunc: true }, code: 400 }
+        {
+          name: 'func response example',
+          description: 'example of func that return object response',
+          exec: (req) => {
+            return { data: { isFunc: true }, code: 400 }
+          },
+          type: 'func',
         },
-        type: 'func',
-      },
-    ],
-  },
-]
+      ],
+    },
+  ],
+}
 
-module.exports = routes
+module.exports = parent
